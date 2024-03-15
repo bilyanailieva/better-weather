fbc7a128f0b530e97bd77e001482e42d9a5a1857
Bilyana Ilieva <b.ilieva@blubito.com>
2024-03-15 13:47:00 +0200

    add and setup mobx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+// import { faHouse } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import "./App.css";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import { ExampleChart } from "./components/ExampleChart";
+import { rootStoreContext } from "./main";
+import { useContext } from "react";
+import { observer } from "mobx-react-lite";
 
-function App() {
+export const App = observer(() => {
+  const rootStore = useContext(rootStoreContext);
+  
   return (
     <div className="fixed flex flex-col top-0 left-0 h-screen w-screen">
       <Navbar />
@@ -10,11 +18,11 @@ function App() {
         <Menu />
         <div className="divider divider-horizontal"></div>
         <div className="grid w-full h-full flex-grow bg-base-300 rounded-box place-items-center">
-          <button type="button" className="btn btn-secondary">Hellos</button>
+          {rootStore.activeTab === 'chart' ? <ExampleChart /> : <>Hello friend!</>}
         </div>
       </div>
     </div>
   );
-}
+});
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { createContext } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import RootStore from "./observable/RootStore.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const rootStore = new RootStore();
+export const rootStoreContext = createContext(rootStore);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <rootStoreContext.Provider value={rootStore}>
+      <App />
+    </rootStoreContext.Provider>
+  </React.StrictMode>
+);
