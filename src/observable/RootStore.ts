1357bbfc7f9ff4ec3cfad6d76a97ef5761deb983
Bilyana Ilieva <b.ilieva@blubito.com>
2024-03-15 14:30:52 +0200

    fix deploy

--- src/observable/RootStore.ts
+++ src/observable/RootStore.ts
@@ -1,16 +1,16 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable } from 'mobx';
 
 export default class RootStore {
-    constructor() {
-        makeAutoObservable(this, {}, { autoBind: true });
-    }
-    private _tab = '';
+  constructor() {
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
+  private _tab = '';
 
-    public setActiveTab(tab: string) {
-        this._tab = tab;
-    }
+  public setActiveTab(tab: string) {
+    this._tab = tab;
+  }
 
-    get activeTab() {
-        return this._tab;
-    }
-};
+  get activeTab() {
+    return this._tab;
+  }
+}
